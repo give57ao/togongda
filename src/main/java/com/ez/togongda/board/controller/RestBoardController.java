62c966dbf37737aa5c5df12a6265dcfbb3b819d0
Yuni <give57ao@gmail.com>
2022-08-08 21:01:06 +0900

    Feat : json -> map 변경 메서드 추가, ajax 출력 완료

--- src/main/java/com/ez/togongda/board/controller/RestBoardController.java
+++ src/main/java/com/ez/togongda/board/controller/RestBoardController.java
@@ -6,23 +6,25 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.json.XML;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ez.togongda.board.model.BoardModel;
-import com.ez.togongda.board.model.CovidModel;
 import com.ez.togongda.board.service.BoardService;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 import com.google.gson.JsonParser;
 
 @RestController
@@ -34,49 +36,80 @@ public class RestBoardController {
 	@PostMapping("/getBoardList")
 	public List<BoardModel> getBoardList() throws IOException, ParseException {
 		List<BoardModel> result = boardService.boardList();
-		
-		
-		String xml = getCovidData();		
-		Object json = XML.toJSONObject(xml); //xml -> json
-        String jsonStr = json.toString(); // json -> string
-        System.out.println("xml : " + xml);
-        System.out.println("===========================================");
-        System.out.println("json : " + jsonStr); 
-    
-        JsonParser jsonParser = new JsonParser();
-        JsonObject jsonObject = (JsonObject)jsonParser.parse(jsonStr); 
-        
-        System.out.println("jsonObject :" + jsonObject);
-        
-        JsonObject covidInfoResponse = (JsonObject)jsonObject.get("response");
-        JsonObject covidInfoBody = (JsonObject)covidInfoResponse.get("body");
-        JsonObject covidInfoItems = (JsonObject)covidInfoBody.get("items");
-        JsonObject covidInfoItem = (JsonObject)covidInfoItems.get("item");
-//        JsonObject gubunEn = (JsonObject)covidInfoItem.get("gubunEn");
 
-        System.out.println("body :" + covidInfoBody);
-        System.out.println("items :" + covidInfoItems);
-        System.out.println("item :" + covidInfoItem);
-//        System.out.println("gubunEn :" + gubunEn);
-        
-        
-        
 		return result;
 	}
 	
 	@PostMapping("/getCovidList")
 	@ResponseBody
-	public ResponseEntity<CovidModel>  getCovidList(HttpServletRequest req) throws IOException{
+	public List<Map<String, Object>> getCovidList(HttpServletRequest req) throws IOException{
 		
 		
 		String xml = getCovidData();		
 		Object json = XML.toJSONObject(xml); //xml -> json
         String jsonStr = json.toString(); // json -> string
-        return new ResponseEntity<CovidModel>(HttpStatus.OK);
+    
+        JsonParser jsonParser = new JsonParser();
+        JsonObject jsonObject = (JsonObject)jsonParser.parse(jsonStr); 
+        
+        
+        JsonObject covidInfoResponse = (JsonObject)jsonObject.get("response");
+        JsonObject covidInfoBody = (JsonObject)covidInfoResponse.get("body"); // pageNo, totalCount
+        JsonObject covidInfoItems = (JsonObject)covidInfoBody.get("items");
+        JsonObject covidInfoItem = (JsonObject)covidInfoItems.get("item"); //여기있는 아이템을 covid model에 넣어야 함
+
+        System.out.println("================================================================");
+        System.out.println("pageNo :" + covidInfoBody.get("pageNo"));
+        System.out.println("totalCount :" + covidInfoBody.get("totalCount"));
+        System.out.println("================================================================");
+        System.out.println("stdDay (기준일자) :" + covidInfoItem.get("stdDay") );
+        System.out.println("deathCnt (누적 사망자 수):" + covidInfoItem.get("deathCnt") );
+        System.out.println("defCnt (누적 확진자 수) :" + covidInfoItem.get("defCnt") );
+        System.out.println("isolClearCnt (누적 격리해제 수) :" + covidInfoItem.get("isolClearCnt") );
+        System.out.println("localOccCnt (지역발생수) :" + covidInfoItem.get("localOccCnt") );
+        System.out.println("qurRate (만명당 발생 확률) :" + covidInfoItem.get("qurRate") );
+        System.out.println("overFlowCnt (해외 유입 수) :" + covidInfoItem.get("overFlowCnt") );
+        System.out.println("incDec (전일 대비 확진자 증감 수) :" + covidInfoItem.get("incDec") );
+        System.out.println("isolIngCnt (누적 격리 해제 수) :" + covidInfoItem.get("isolIngCnt") );
+        System.out.println("gubun (시도 명)  :" + covidInfoItem.get("gubun") );
+        System.out.println("================================================================");
+        
+        Map<String, Object> map = getMapFromJsonObject(covidInfoItem); //json데이터 -> map으로 바꾸는 메서드 실행 후 map 변수에 저장
+        List<Map<String, Object>> result = null; //리스트 선언
+
+		result = new ArrayList<>(); 
+		result.add(map); //저장된 map에 리스트를 넣고
+
+        return result; //ajax에게 넘겨줌 
 	}
 	
+	/**
+	 * @param JSONObject
+	 * @apiNote JSONObject를 Map<String, String> 형식으로 변환처리.
+	 * @return Map<String,String>
+	 * **/
+	@SuppressWarnings("unchecked")
+	public static Map<String, Object> getMapFromJsonObject(JsonObject covidInfoItem){
+	    Map<String, Object> map = null;
+	    
+	    try {
+	       map = new ObjectMapper().readValue(covidInfoItem.toString(), Map.class);
+	    } catch (JsonParseException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	    } catch (JsonMappingException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	    } catch (IOException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	    }
+	    return map;
+	}
+
 	
-	// 공공 데이터를 받아오는 메서드
+	
+	// 공공 데이터를 받아오는 메서드 (나중에 stdDay를 변수로 변경해서 요청 전에 값을 변경할 때 사용해야 함)
 	public String getCovidData() throws IOException {
 		
 		StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1352000/ODMS_COVID_04/callCovid04Api"); /*URL*/
--- src/main/webapp/WEB-INF/views/jsp/board.jsp
+++ src/main/webapp/WEB-INF/views/jsp/board.jsp
@@ -40,16 +40,16 @@
 						scrollY : false,
 					
 						columns : [ {
-							header : 'board_title',
+							header : '게시글 제목',
 							name : 'board_title'
 						}, {
-							header : 'board_date',
+							header : '작성 날짜',
 							name : 'board_date'
 						}, {
-							header : 'board_no',
+							header : '글 번호',
 							name : 'board_no'
 						}, {
-							header : 'board_file_check',
+							header : '첨부파일 유무',
 							name : 'board_file_check'
 						} ]
 
@@ -59,17 +59,61 @@
 				}// suc
 			}); //ajax 
 			
-/* 			$.ajax({
-		        type: "POST",
-		        url : "togongda/getCovidList",
-		        headers: {'Content-Type': 'application/json'},
+			//공공 데이터
+			$.ajax({
+				url : "/togongda/getCovidList",
+				method : "POST",
 				dataType : "JSON",
-		        success : function (data){
-		        	console.log(data);
-		        },
-		        error : function(e){
-		        }
-		}) */
+				contentType : "application/json; charset=UTF-8",
+				success : function(result) {
+					console.dir(result);
+
+					var grid1 = new tui.Grid({
+						el : document.getElementById('grid1'),
+						scrollX : false,
+						scrollY : false,
+					
+						columns : [ {
+							header : '기준일자',
+							name : 'stdDay'
+						}, {
+							header : '누적 사망자 수',
+							name : 'deathCnt'
+						}, {
+							header : '누적 확진자 수',
+							name : 'defCnt'
+						}, {
+							header : '누적 격리해제 수',
+							name : 'isolClearCnt'
+						}, {
+							header : '지역발생수',
+							name : 'localOccCnt'
+						}, {
+							header : '만명당 발생 확률',
+							name : 'qurRate'
+						}, {
+							header : '해외 유입 수',
+							name : 'overFlowCnt'
+						}, {
+							header : '전일 대비 확진자 증감 수',
+							name : 'incDec'
+						}, {
+							header : '누적 격리 해제 수',
+							name : 'isolIngCnt'
+						}, {
+							header : '시도명',
+							name : 'gubun'
+						}
+						
+						]
+
+					}); //grid 
+
+					grid1.resetData(result);
+				}// suc
+			}); //ajax 
+			
+
 			
 			
 			
@@ -87,6 +131,8 @@
 
 	<!-- 토스트 그리드 -->
 	<div id="grid"></div>
+	
+	<div id="grid1"></div>
 	
 	<!-- 페이지네이션 -->
 	<div id="pagination2" class="tui-pagination"></div>
