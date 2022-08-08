194e56a5dc56eb7a888806c3321af8d336bca5d8
Yuni <give57ao@gmail.com>
2022-08-08 19:09:38 +0900

    누락사항 커밋

---
+++ src/main/java/com/ez/togongda/board/model/CovidModel.java
@@ -0,0 +1,119 @@
+package com.ez.togongda.board.model;
+
+import java.util.Date;
+
+
+
+public class CovidModel {
+
+private int pageNo;
+private int totalCount;
+private String gubun;
+private int deathCnt;
+private int defCnt;
+private int isolClearCnt;
+private Date stdDay;
+private int localOccCnt;
+private int qurRate;
+private int overFlowCnt;
+private String gubunCn;
+private int incDec;
+private int isolIngCnt;
+private int numOfRows;
+private String gubunEn;
+
+
+public int getPageNo() {
+	return pageNo;
+}
+public void setPageNo(int pageNo) {
+	this.pageNo = pageNo;
+}
+public int getTotalCount() {
+	return totalCount;
+}
+public void setTotalCount(int totalCount) {
+	this.totalCount = totalCount;
+}
+public String getGubun() {
+	return gubun;
+}
+public void setGubun(String gubun) {
+	this.gubun = gubun;
+}
+public int getDeathCnt() {
+	return deathCnt;
+}
+public void setDeathCnt(int deathCnt) {
+	this.deathCnt = deathCnt;
+}
+public int getDefCnt() {
+	return defCnt;
+}
+public void setDefCnt(int defCnt) {
+	this.defCnt = defCnt;
+}
+public int getIsolClearCnt() {
+	return isolClearCnt;
+}
+public void setIsolClearCnt(int isolClearCnt) {
+	this.isolClearCnt = isolClearCnt;
+}
+public Date getStdDay() {
+	return stdDay;
+}
+public void setStdDay(Date stdDay) {
+	this.stdDay = stdDay;
+}
+public int getLocalOccCnt() {
+	return localOccCnt;
+}
+public void setLocalOccCnt(int localOccCnt) {
+	this.localOccCnt = localOccCnt;
+}
+public int getQurRate() {
+	return qurRate;
+}
+public void setQurRate(int qurRate) {
+	this.qurRate = qurRate;
+}
+public int getOverFlowCnt() {
+	return overFlowCnt;
+}
+public void setOverFlowCnt(int overFlowCnt) {
+	this.overFlowCnt = overFlowCnt;
+}
+public String getGubunCn() {
+	return gubunCn;
+}
+public void setGubunCn(String gubunCn) {
+	this.gubunCn = gubunCn;
+}
+public int getIncDec() {
+	return incDec;
+}
+public void setIncDec(int incDec) {
+	this.incDec = incDec;
+}
+public int getIsolIngCnt() {
+	return isolIngCnt;
+}
+public void setIsolIngCnt(int isolIngCnt) {
+	this.isolIngCnt = isolIngCnt;
+}
+public int getNumOfRows() {
+	return numOfRows;
+}
+public void setNumOfRows(int numOfRows) {
+	this.numOfRows = numOfRows;
+}
+public String getGubunEn() {
+	return gubunEn;
+}
+public void setGubunEn(String gubunEn) {
+	this.gubunEn = gubunEn;
+}
+
+
+
+}
